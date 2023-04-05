401404ec8e2a97433ad21aa6e31bc82bb4c1f012
Yu Li <liyu1981@gmail.com>
2023-04-05 03:28:56 +0000

    add wobserver-ng to deps

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Pooly.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:wobserver_ng, "~> 1.14"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
